99077741ab2fea7afcca6d2d8b8ed5f863b01e2b
nguyenvietngocquang <nguyenvietngocquang@gmail.com>
2021-04-16 02:11:55 +0700

    WDPSO

--- WDPSO/src/graph/MakeWeightedGraph.java
+++ WDPSO/src/graph/MakeWeightedGraph.java
@@ -14,9 +14,14 @@ public class MakeWeightedGraph {
 			LinkedList<Line> lines) {
 		this.weights = new double[visitPoints.size()][visitPoints.size()];
 		for (int i = 0; i < visitPoints.size(); i++) {
-			for (int j = 0; j < visitPoints.size(); j++) {
-				AStar findPath = new AStar(myGraph, midPoints, lines, visitPoints.get(i), visitPoints.get(j));
-				weights[i][j] = findPath.length;
+			for (int j = i; j < visitPoints.size(); j++) {
+				if (i == j) {
+					weights[i][j] = 0;
+				} else {
+					AStar findPath = new AStar(myGraph, midPoints, lines, visitPoints.get(i), visitPoints.get(j));
+					weights[i][j] = findPath.length;
+					weights[j][i] = findPath.length;
+				}
 			}
 		}
 	}
