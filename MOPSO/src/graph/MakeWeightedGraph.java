847f44bf2ccc22269c24469a6d798220b937920d
Nguyễn Viết Ngọc Quang <nguyenvietngocquang@gmail.com>
2021-06-18 00:47:59 +0700

    MOPSO: Done

--- MOPSO/src/graph/MakeWeightedGraph.java
+++ MOPSO/src/graph/MakeWeightedGraph.java
@@ -12,9 +12,10 @@ public class MakeWeightedGraph {
 
 	public MakeWeightedGraph(Graph myGraph, LinkedList<Point> visitPoints, LinkedList<Point> midPoints,
 			LinkedList<Line> lines) {
-		this.weights = new double[visitPoints.size()][visitPoints.size()];
-		for (int i = 0; i < visitPoints.size(); i++) {
-			for (int j = 0; j < visitPoints.size(); j++) {
+		int size = visitPoints.size();
+		this.weights = new double[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				AStar findPath = new AStar(myGraph, midPoints, lines, visitPoints.get(i), visitPoints.get(j));
 				weights[i][j] = findPath.length;
 			}
